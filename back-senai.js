877bfc49e2f11cefff3ce300c2b28cde4e9c3ffe
Julio-sinistro69 <juliodesouza2077@gmail.com>
2025-09-17 11:06:53 -0300

    Add files via upload

--- back-senai.js
+++ back-senai.js
@@ -36,3 +36,73 @@ app.post("/usuarios", async (req, res) => {
     email
     });
 });
+
+//selecionar um usuario
+app.get('usuarios/:id', (req,res) => {
+    let idUsuario = req.paramas.id;
+
+    db.get(`SELECT id, nome, email FROM usuarios 
+    WHERE id = ?`,
+    [idUsuario], (err, row) => {
+        if (row) {
+            res.json(row)
+        } else{
+            res.status(404).json({
+                'message' : 'Usuario não encontrado.'
+            })
+        }
+    })
+})
+
+//deletar usuarios
+app.delete('usuarios/:id', (req, res) =>{
+    let idUsuario = req.params.id
+
+    db.get(`DELETE FROM usuarios WHERE id = ?`,
+    [idUsuario], function(err, result) {
+        if (result) {
+            res.json({
+            'masage' : 'Usuario deletado'
+        })
+        } else{
+            res.status(404).json({
+                'masage' : 'Usuario não encontrado'
+            })
+        }
+    })
+        
+    })
+    
+// Editar usuário
+app.put("/usuarios/:id", async (req, res) => {
+    let idUsuario = req.params.id;  
+
+    let nome = req.body.nome;
+    let email = req.body.email;
+    let senha = req.body.senha;
+
+    let senhaHash = await bcrypt.hash(senha, 10);
+
+    db.run(
+        `UPDATE usuarios SET nome = ?, email = ?, senha = ? WHERE id = ?`,
+        [nome, email, senhaHash, idUsuario],
+        function() {
+            res.json({
+
+            });
+        }
+    );
+});
+
+
+
+//listar todos os usuarios
+
+app.get("/usuarios",(req, res) =>{
+    db.all(`SELECT id, nome, email FROM usuarios`, [], (err, rows)=>{
+        res.json(rows)
+    })
+})
+
+//Iniciar o server
+app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`))
